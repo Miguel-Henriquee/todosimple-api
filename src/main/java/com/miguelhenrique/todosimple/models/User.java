b2a3962889dac96342eb935d3c37ab40225d18da
Miguel-Henrique <miguenrique27@gmail.com>
2024-06-27 07:59:07 -0300

    Create Task model

--- src/main/java/com/miguelhenrique/todosimple/models/User.java
+++ src/main/java/com/miguelhenrique/todosimple/models/User.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -46,7 +47,8 @@ public class User {
     @Size(groups = { CreateUser.class, UpdateUser.class }, min = 8, max = 60)
     private String password;
 
-    // private List<Task> tasks = new ArrayList<Task>();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
     public User() {
     }
@@ -94,6 +96,14 @@ public class User {
     public User password(String password) {
         setPassword(password);
         return this;
+    }
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     @Override
