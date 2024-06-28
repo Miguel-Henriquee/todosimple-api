35e8650cba25a9a0ead4101af5c1bd5cd350c2d1
Miguel-Henrique <miguenrique27@gmail.com>
2024-06-28 15:16:53 -0300

    Add password encoder and create ProfileEnum

--- src/main/java/com/miguelhenrique/todosimple/services/UserSevice.java
+++ src/main/java/com/miguelhenrique/todosimple/services/UserSevice.java
@@ -1,12 +1,16 @@
 package com.miguelhenrique.todosimple.services;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.miguelhenrique.todosimple.models.User;
+import com.miguelhenrique.todosimple.models.enums.ProfileEnum;
 import com.miguelhenrique.todosimple.repositories.UserRepository;
 import com.miguelhenrique.todosimple.services.exceptions.DataBindingViolationException;
 import com.miguelhenrique.todosimple.services.exceptions.ObjectNotFoundException;
@@ -14,6 +18,9 @@ import com.miguelhenrique.todosimple.services.exceptions.ObjectNotFoundException
 @Service
 public class UserSevice {
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    
     @Autowired
     private UserRepository userRepository;
 
@@ -27,6 +34,8 @@ public class UserSevice {
     @Transactional
     public User create(User obj) {
         obj.setId(null);
+        obj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
+        obj.setProfiles(Stream.of(ProfileEnum.USER.getCode()).collect(Collectors.toSet()));
         obj = this.userRepository.save(obj);
         return obj;
     }
@@ -35,6 +44,7 @@ public class UserSevice {
     public User update(User obj) {
         User newObj = findById(obj.getId());
         newObj.setPassword(obj.getPassword());
+        newObj.setPassword(this.bCryptPasswordEncoder.encode(obj.getPassword()));
         return this.userRepository.save(newObj);
     }
 
